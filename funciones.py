08f1825ca8c42c1c9aacde93f7ff77e37cc4534c
Matias Lesbegueris <milesbegueris@gmail.com>
2026-05-18 12:59:37 -0300

    validaciones de alta de producto

--- funciones.py
+++ funciones.py
@@ -21,4 +21,24 @@ def altaDeProducto():
             print('numero')
         else:
             print('error')
-    
+    nombre=input('escriba el nombre del nuevo producto: ')
+    while len(nombre)==0:
+        nombre=input('ERROR: escriba el nombre del nuevo producto: ')
+        #añadir producto a lista
+    unidad=input('escriba la unidad de medicion del producto (kg, lt, mts): ')
+    while unidad!="kg" and unidad!= 'lt' and unidad== 'mts':
+        unidad=input('ERROR: unidad erronea, escriba una unidad valida (kg, lt, mts): ')
+
+    stockA=int(input('escriba el stock actual del producto: '))
+    while stockA<=0:
+        stockA=int(input('ERROR: escriba un stock valido: '))
+    stockM=int(input('escriba el stock minimo requerido para el producto: '))
+    while stockM<=0:
+        stockM=int(input('ERROR: escriba un stock minimo valido: '))
+        if stockM>stockA:
+            stockM=int(input('ERROR: el stock minimo no puede ser mayor que el stock actual, intente otra vez:  '))
+    costo=int(input('escriba el costo unitario del producto: '))
+    while costo<=0:
+        costo=int(input('ERROR: escriba un costo por unidad valido: '))
+
+
