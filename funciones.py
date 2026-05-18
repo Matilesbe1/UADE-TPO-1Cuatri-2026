a1c9c4939edbb55c30838692f05d4a97b2803301
Matias Lesbegueris <milesbegueris@gmail.com>
2026-05-18 10:37:54 -0300

    prueba

--- funciones.py
+++ funciones.py
@@ -1,3 +1,11 @@
 def ValidarProducto(codigo, nombre, unidad, stockActual, stockMinimo, costo):
-    if 
+    
+    if nombre.length==0:
+        print('el nombre del producto esta vacio')
+    if stockActual<=0:
+        print('el stock actual es invalido')
+    if stockMinimo<=0:
+        print('el stock minimo es invalido')
+    if costo<=0:
+        print('el costo unitario es invalido')
     return 
