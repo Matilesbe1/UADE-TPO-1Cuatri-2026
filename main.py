28cceb0339fe495c6f300100a70f97ae4a722156
Lorenzo <rossilorenzo2602@gmail.com>
2026-05-18 20:00:44 -0300

    Subo main de prueba

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-codigo=[123]
-nombre=['hola']
-unida=['kg']
-stockA=[2]
-stockM=[1]
-costo=[500]
+#Programa principal
+import funciones #form branch lolo1
 
-import funciones
+codigo=[111,222,333,444]
+nombre=["azucar","cinta","moto","cerveza"]
+unidad=['kg',"mts", "u","lts"]
+stockA=[50,100,5,25]
+stockM=[5,10,1,2]
+costo=[100,50,500,250]
+
 def main():
-    funciones.ValidarProducto()
+    funciones.modificarProducto(codigo,nombre,unidad,stockA,stockM,costo)
+    for i in range(len(codigo)):
+        print(codigo[i],nombre[i],unidad[i],stockA[i],stockM[i],costo[i])
+main()
