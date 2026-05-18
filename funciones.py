23d39615386c8a6d40dadeb613167f3c95a5b5e2
Lorenzo <rossilorenzo2602@gmail.com>
2026-05-18 11:00:42 -0300

    funciones 2

--- funciones.py
+++ funciones.py
@@ -1,6 +1,17 @@
 def ValidarProducto(codigo, nombre, unidad, stockActual, stockMinimo, costo):
     
+    if nombre.length==0:
+        print('el nombre del producto esta vacio')
+    if stockActual<=0:
+        print('el stock actual es invalido')
+    if stockMinimo<=0:
+        print('el stock minimo es invalido')
+    if costo<=0:
+        print('el costo unitario es invalido')
     return 
 
 def modificarProducto(lst):
+    print("hola")
+
+def bajaProductos(lst):
     print("hola")
